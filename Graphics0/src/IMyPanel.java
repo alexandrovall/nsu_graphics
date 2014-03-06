e760759536b7bef05dbbf740e80363eea24f45ba
alexandrovall <alexandrovall@yandex.ru>
2014-03-06 21:00:38 +0700

    Fix vol.3.1

---
+++ Graphics0/src/IMyPanel.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+/**
+ * Interface for correct drawing
+ */
+public interface IMyPanel
+{
+    /**
+     *
+     * @return the size of my object
+     */
+    public Dimension getSizeOfPanel();
+}
